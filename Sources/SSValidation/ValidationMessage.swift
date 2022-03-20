2a55f5c5d09257542e8330c7f126fa023cf0fdd7
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2022-03-20 20:07:48 +0100

    String validations

--- Sources/SSValidation/ValidationMessage.swift
+++ Sources/SSValidation/ValidationMessage.swift
@@ -7,32 +7,23 @@
 
 import Foundation
 
-public enum ValidationMessage {
-    case empty
-    case invalid
-    case tooSmall(Double)
-    case tooBig(Double)
-    case tooShort(Int)
-    case tooLong(Int)
+public struct ValidationMessage {
 
-    public var message: String {
-        switch self {
-        case .empty:
-            return "Value can not be empty."
-        case .invalid:
-            return "Value is invalid."
-        case let .tooSmall(minValue):
-            return "Value must be at least \(minValue.asString)."
-        case let .tooBig(maxValue):
-            return "Value can not be bigger than \(maxValue.asString)."
-        case let .tooShort(minLength):
-            return "Text must have at least \(minLength) characters."
-        case let .tooLong(maxLength):
-            return "Text can have maximum \(maxLength) characters."
-        }
-    }
+//        case let .tooSmall(minValue):
+//            return "Value must be at least \(minValue.asString)."
+//        case let .tooBig(maxValue):
+//            return "Value can not be bigger than \(maxValue.asString)."
+//        }
+//    }
+
+    public static let empty = "Field can not be empty."
+    public static let invalid = "Text is invalid."
 
     public static func tooShort(_ minLength: Int) -> String {
         "Text must have at least \(minLength.asString) characters."
+    }
+
+    public static func tooLong(_ maxLength: Int) -> String {
+        "Text can have maximum \(maxLength.asString) characters."
     }
 }
--- Sources/SSValidation/Validator/Validator+String.swift
+++ Sources/SSValidation/Validator/Validator+String.swift
@@ -1,6 +1,6 @@
 //
 //  Validator+String.swift
-//  
+//  SSValidation
 //
 //  Created by sebastianstaszczyk on 20/03/2022.
 //
@@ -9,11 +9,32 @@ import Foundation
 
 public extension Validator where Source == String {
 
-    static func notEmpty(_ message: @autoclosure @escaping () -> String = ValidationMessage.empty.message) -> Validator {
+    static func notEmpty(_ message: @autoclosure @escaping () -> String = ValidationMessage.empty) -> Validator {
         Validator { $0.isEmpty ? .invalid(msg: message()) : .valid }
     }
 
-    static func minLength( _ minLength: Int, tooShortMessage: @escaping (Int) -> String = ValidationMessage.tooShort) -> Validator {
+    static func isEqual(to comparedText: String, errorMessage: @autoclosure @escaping () -> String) -> Validator {
+        Validator { $0 != comparedText ? .invalid(msg: errorMessage()) : .valid }
+    }
+
+    static func matches(regex: String, errorMessage: @autoclosure @escaping () -> String = ValidationMessage.invalid) -> Validator {
+        Validator { !NSPredicate(format: "SELF MATCHES %@", regex).evaluate(with: $0) ? .invalid(msg: errorMessage()) : .valid }
+    }
+
+    static func minLength(_ minLength: Int, tooShortMessage: @escaping (Int) -> String = ValidationMessage.tooShort) -> Validator {
         Validator { $0.count < minLength ? .invalid(msg: tooShortMessage(minLength)) : .valid }
+    }
+
+    static func maxLength(_ maxLength: Int, tooLongMessage: @escaping (Int) -> String = ValidationMessage.tooLong) -> Validator {
+        Validator { $0.count > maxLength ? .invalid(msg: tooLongMessage(maxLength)) : .valid }
+    }
+
+    static func lengthBetween(
+        _ range: ClosedRange<Int>,
+        tooShortMessage: @escaping (Int) -> String = ValidationMessage.tooShort,
+        tooLongMessage: @escaping (Int) -> String = ValidationMessage.tooLong
+    ) -> Validator {
+        minLength(range.lowerBound, tooShortMessage: tooShortMessage)
+            .and(maxLength(range.upperBound, tooLongMessage: tooLongMessage))
     }
 }
