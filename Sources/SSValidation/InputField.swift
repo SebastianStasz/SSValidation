75fcd1552f0dc0dbfe18c23504f3713548496839
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2022-09-02 21:54:19 +0200

    Input field improvements & mapping value

--- Sources/SSValidation/InputField.swift
+++ Sources/SSValidation/InputField.swift
@@ -13,18 +13,74 @@ public struct InputField<T>: View {
 
     private let title: String
     private let prompt: Text?
+    private let mapValue: ((T) -> String)?
     private let isSecure: Bool
     private let keyboardType: UIKeyboardType
 
     public init(
         _ title: String,
         viewModel: InputVM<T>,
-        prompt: String? = nil,
+        prompt: Text,
+        mapValue: ((T) -> String)? = nil,
         isSecure: Bool = false,
         keyboardType: UIKeyboardType? = nil
     ) {
         self.title = title
         self.viewModel = viewModel
+        self.prompt = prompt
+        self.mapValue = mapValue
+        self.isSecure = isSecure
+        self.keyboardType = keyboardType ?? viewModel.defaultKeyboardType
+    }
+
+    public var body: some View {
+        textField
+            .focused($isFocued)
+            .keyboardType(keyboardType)
+            .onChange(of: viewModel.textInput, perform: viewModel.textChanged)
+            .onChange(of: isFocued, perform: focusChanged)
+    }
+
+    @ViewBuilder
+    private var textField: some View {
+        if isSecure {
+            SecureField(title, text: textFieldValue, prompt: prompt)
+        } else {
+            TextField(title, text: textFieldValue, prompt: prompt)
+        }
+    }
+
+    private var textFieldValue: Binding<String> {
+        Binding(
+            get: { getStringForTextField() },
+            set: { viewModel.textInput = $0 }
+        )
+    }
+
+    private func getStringForTextField() -> String {
+        if !isFocued, let mapInput = mapValue, let value = viewModel.resultValue {
+            return mapInput(value)
+        }
+        return viewModel.textInput
+    }
+
+    private func focusChanged(_ isFocued: Bool) {
+        viewModel.textInput = viewModel.textInput.trim
+    }
+}
+
+extension InputField {
+    public init(
+        _ title: String,
+        viewModel: InputVM<T>,
+        prompt: String? = nil,
+        mapValue: ((T) -> String)? = nil,
+        isSecure: Bool = false,
+        keyboardType: UIKeyboardType? = nil
+    ) {
+        self.title = title
+        self.viewModel = viewModel
+        self.mapValue = mapValue
         self.isSecure = isSecure
         self.keyboardType = keyboardType ?? viewModel.defaultKeyboardType
 
@@ -32,36 +88,6 @@ public struct InputField<T>: View {
             self.prompt = Text(prompt)
         } else {
             self.prompt = nil
-        }
-    }
-
-    public var body: some View {
-        Group {
-            if isSecure {
-                SecureField(title, text: $viewModel.textInput, prompt: prompt)
-            } else {
-                TextField(title, text: $viewModel.textInput, prompt: prompt)
-            }
-        }
-        .keyboardType(keyboardType)
-//        .focused($isFocued)
-//        .onSubmit(of: .text, didSubmit)
-        .onChange(of: viewModel.textInput, perform: viewModel.textChanged(to:))
-        .onChange(of: isFocued, perform: focusChanged)
-    }
-
-//    private func didSubmit() {
-//        if viewModel.validationState.isValid {
-//            isFocued = false
-//        } else {
-//            viewModel.textChanged(to: viewModel.textInput)
-//            isFocued = true
-//        }
-//    }
-
-    private func focusChanged(_ isFocued: Bool) {
-        if !isFocued {
-            viewModel.textInput = viewModel.textInput.trim
         }
     }
 }
